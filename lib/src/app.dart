6e2f462870fff54346df4a425b9b7e46c0ae5a61
Kitto Hernandez <kitto.hernandes@gmail.com>
2021-09-16 13:59:43 +0800

    Stream Successful

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,8 @@
+import 'package:cravv_0_1/src/models/customer.dart';
+import 'package:cravv_0_1/src/services/authentication_service.dart';
 import 'package:cravv_0_1/src/wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -7,8 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
-    return MaterialApp(
-      home: const Wrapper(),
+    return StreamProvider<Customer?>.value(
+      initialData: null,
+      value: AuthService().customer,
+      child: MaterialApp(
+        home: const Wrapper(),
+      ),
     );
   }
 }
--- lib/src/wrapper.dart
+++ lib/src/wrapper.dart
@@ -1,12 +1,17 @@
-import 'package:cravv_0_1/src/screens/authenticate/authenticate.dart';
-import 'package:cravv_0_1/src/screens/home/home.dart';
+import 'package:cravv_0_1/src/models/customer.dart';
+import 'package:cravv_0_1/src/screens/authenticate/authenticate_page.dart';
+import 'package:cravv_0_1/src/screens/home/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final customer = Provider.of<Customer?>(context);
+    print(customer);
+
     // return either Home or Authenticate widget
     // ignore: prefer_const_constructors
     return Authenticate();
