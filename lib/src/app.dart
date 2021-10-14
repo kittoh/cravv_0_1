a19f8c714859a6d049aeca5f15c12695d6af9950
Kitto Hernandez <kitto.hernandes@gmail.com>
2021-10-15 00:07:52 +0800

    commit

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,6 @@
-import 'package:cravv_0_1/src/screens/authenticate/log_in_page.dart';
-import 'package:cravv_0_1/src/screens/authenticate/register_page.dart';
-import 'package:cravv_0_1/src/screens/home/home_page.dart';
+import 'package:cravv_0_1/src/screens/authenticate/login_screen.dart';
+import 'package:cravv_0_1/src/screens/authenticate/register_screen.dart';
+import 'package:cravv_0_1/src/screens/home/home_screen.dart';
 import 'package:cravv_0_1/src/services/authentication_service.dart';
 import 'package:cravv_0_1/src/wrapper.dart';
 import 'package:flutter/material.dart';
@@ -24,9 +24,9 @@ class MyApp extends StatelessWidget {
         initialRoute: '/',
         routes: {
           '/': (context) => Wrapper(),
-          '/login': (context) => LogInPage(),
-          '/register': (context) => RegisterPage(),
-          '/home': (context) => HomePage(),
+          '/login': (context) => LoginScreen(),
+          '/register': (context) => RegisterScreen(),
+          '/home': (context) => HomeScreen(),
         },
       ),
     );
